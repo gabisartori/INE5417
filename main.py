08c0f1bf8c14f571d65de0d6ac2eba05f432ff19
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-04-16 00:16:07 -0300

    Trocar linhas das bordas por triângulos entre os hexágonos

--- main.py
+++ main.py
@@ -158,11 +158,11 @@ class HexInterface:
         self._root.iconphoto(False, tk.PhotoImage(file='assets/logo.png'))
 
         # Math for drawing cells
-        self.__side_bar_width = 5
+        self.__canvas_padding = 15
         self._canvas_size_x = 800
         self._hex_side_size = self._canvas_size_x / (1.5*((self._game.size)*3**(1/2)))
-        self._canvas_size_x += 2*self.__side_bar_width
-        self._canvas_size_y = 1.5*self._hex_side_size * self._game.size + self._hex_side_size/2 + 2*self.__side_bar_width
+        self._canvas_size_x += 2*self.__canvas_padding
+        self._canvas_size_y = 1.5*self._hex_side_size * self._game.size + self._hex_side_size/2 + 2*self.__canvas_padding
 
         # Control gamestate
         self._ready_players = 0
@@ -218,56 +218,93 @@ class HexInterface:
         # Board
         self.__canvas.grid(row=2, column=1, padx=10, pady=10)
 
-        # Draw top and bottom bars for local player
-        bar_size = self._hex_side_size*3**(1/2)*self._game.size
-        bottom_bar_offset = bar_size/2 - self._hex_side_size/2
-        top_bar_coords = (
-            0, self.fix_y(0),
-            bar_size, self.fix_y(self.__side_bar_width),
-        )
-        bottom_bar_coords = (
-            bottom_bar_offset, self.fix_y(self._canvas_size_y - self.__side_bar_width),
-            bottom_bar_offset + bar_size, self.fix_y(self._canvas_size_y),
-        )
+        [self.draw_triangle(i, True, self._game.local_player) for i in range(self._game.size+1)]
+        [self.draw_triangle(i, False, self._game.local_player) for i in range(self._game.size+1)]
 
-        left_bar_coords = (
-            -self._hex_side_size*3**(1/2)/2, self.fix_y(0),
-            bottom_bar_offset, self.fix_y(self._canvas_size_y)
-        )
-
-        right_bar_coords = (
-            bar_size, self.fix_y(0),
-            self.__side_bar_width + bar_size + self._game.size*self._hex_side_size*3**(1/2)/2, self.fix_y(self._canvas_size_y)
-        )
-
-        # Draw side bars for each player goals
-        if self._game.local_player != None:
-            self.__canvas.create_rectangle(*top_bar_coords, fill=self._game.local_player.color)
-            self.__canvas.create_rectangle(*bottom_bar_coords, fill=self._game.local_player.color)
-
-        if self._game.remote_player != None:
-            self.__canvas.create_line(*left_bar_coords, fill=self._game.remote_player.color, width=self.__side_bar_width)
-            self.__canvas.create_line(*right_bar_coords, fill=self._game.remote_player.color, width=self.__side_bar_width)
+        [self.draw_triangle(i, True, self._game.remote_player) for i in range(1, self._game.size)]
+        [self.draw_triangle(i, False, self._game.remote_player) for i in range(1, self._game.size)]
 
     def draw_hexagon(self, i, j, color, edgecolor='black'):
-        side_root3_half = 3**(1/2)*self._hex_side_size/2
-        x = 2*i*side_root3_half + j*side_root3_half
+        side_root3 = 3**(1/2)*self._hex_side_size
+        x = i*side_root3 + j*side_root3/2
         y = 1.5*j*self._hex_side_size
 
-        x += self.__side_bar_width
-        y += self.__side_bar_width
+        x += self.__canvas_padding
+        y += self.__canvas_padding
 
         self.__canvas.create_polygon(
-            x + side_root3_half, self.fix_y(y),
-            x + 2*side_root3_half, self.fix_y(y + self._hex_side_size/2),
-            x + 2*side_root3_half, self.fix_y(y + 3*self._hex_side_size/2),
-            x + side_root3_half, self.fix_y(y + 2*self._hex_side_size),
+            x + side_root3/2, self.fix_y(y),
+            x + side_root3, self.fix_y(y + self._hex_side_size/2),
+            x + side_root3, self.fix_y(y + 3*self._hex_side_size/2),
+            x + side_root3/2, self.fix_y(y + 2*self._hex_side_size),
             x, self.fix_y(y + 3*self._hex_side_size/2),
             x, self.fix_y(y + self._hex_side_size/2),
 
             fill=color,
             outline=edgecolor,
             tags="hexagon"
+        )
+
+    def draw_triangle(self, i: int, start: bool, player: Player):
+        side_root3 = 3**(1/2)*self._hex_side_size
+        coords = []
+        if player == self._game.local_player:
+            if start: # Bottom side
+                y = self.__canvas_padding
+                x = self.__canvas_padding + i*side_root3
+
+                coords.append(max(self.__canvas_padding, x - side_root3/2))
+                coords.append(self.fix_y(y))                 
+
+                coords.append(x)
+                coords.append(self.fix_y(y + self._hex_side_size/2))
+
+                coords.append(min(x + side_root3/2, self.__canvas_padding+self._game.size*side_root3))
+                coords.append(self.fix_y(y))               
+            else: # Top side
+                start_x = self.__canvas_padding + (self._game.size-1)*side_root3/2
+                x = start_x + i*side_root3
+                y = self._canvas_size_y - self.__canvas_padding
+
+                coords.append(max(start_x, x - side_root3/2))
+                coords.append(self.fix_y(y))
+
+                coords.append(x)
+                coords.append(self.fix_y(y - self._hex_side_size/2))
+
+                coords.append(min(x + side_root3/2, start_x + self._game.size*side_root3))
+                coords.append(self.fix_y(y))
+        else:
+            if start: # Left side
+                x = self.__canvas_padding + i*side_root3/2
+                y = self.__canvas_padding + i*1.5*self._hex_side_size + self._hex_side_size/2
+
+                coords.append(x - side_root3/2)
+                coords.append(self.fix_y(y - self._hex_side_size/2))
+
+                coords.append(x)
+                coords.append(self.fix_y(y))
+
+                coords.append(x)
+                coords.append(self.fix_y(y + self._hex_side_size))
+            else: # Right side
+                start_x = self.__canvas_padding + (self._game.size)*side_root3
+                x = start_x + (i-1)*side_root3/2
+                y = self.__canvas_padding + i*1.5*self._hex_side_size
+                print(y)
+
+                coords.append(x)
+                coords.append(self.fix_y(y))
+
+                coords.append(x)
+                coords.append(self.fix_y(y - self._hex_side_size))
+
+                coords.append(x + side_root3/2)
+                coords.append(self.fix_y(y + self._hex_side_size/2))
+
+        self.__canvas.create_polygon(
+            *coords,
+            fill=player.color,
         )
 
     def player_press_action_button(self, player: Player):
@@ -337,6 +374,9 @@ class HexInterface:
         # j = y/(1.5*side_size)
         # i = (x-j*side_root3_half)/(side_root3_half*2)
         y = self.fix_y(y)
+        x -= self.__canvas_padding
+        y -= self.__canvas_padding
+
         j = int(math.floor(y/(1.5*self._hex_side_size)))
         i = int(math.floor((x-j*self._hex_side_size*3**(1/2)/2)/(self._hex_side_size*3**(1/2))))
         return i, j
